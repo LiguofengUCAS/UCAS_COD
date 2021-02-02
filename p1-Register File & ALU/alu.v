4a0d31cf42a182c6ef67dab900bcb7afe9133739
LiguofengUCAS <liguofeng18@mails.ucas.ac.cn>
2021-02-03 03:21:35 +0800

    fix tab & space

--- p1-Register File & ALU/alu.v
+++ p1-Register File & ALU/alu.v
@@ -17,13 +17,13 @@ module alu(
 	input [`DATA_WIDTH - 1:0]  A,
 	input [`DATA_WIDTH - 1:0]  B,
 	input [2:0              ]  ALUop,
-	output 					   Overflow,
-	output 				       CarryOut,
-	output 					   Zero,
+	output                     Overflow,
+	output                     CarryOut,
+	output                     Zero,
 	output [`DATA_WIDTH - 1:0] Result
 );
 	
-	wire					 ext_A;
+	wire                     ext_A;
 	wire [`DATA_WIDTH    :0] A_tmp;
 	wire [`DATA_WIDTH    :0] B_tmp;
 	wire [`DATA_WIDTH - 1:0] carryout_add;
@@ -44,19 +44,19 @@ module alu(
 	assign sub_result = add_result;
 	assign slt_result[31:1] = 31'b0;
 	assign slt_result[0] = (A[31] & ~B[31]) ||
-			       		   (A[31] ~^ B[31]) && add_result[31];		  
+                           (A[31] ~^ B[31]) && add_result[31];		  
 
 	assign Overflow = !A[31] && !B[31] &&  add_result[31] && ALUop == `ADD ||
-			  		   A[31] &&  B[31] && !add_result[31] && ALUop == `ADD ||
-			  		  !A[31] &&  B[31] &&  sub_result[31] && ALUop == `SUB ||
-			 		   A[31] && !B[31] && !sub_result[31] && ALUop == `SUB  ;
+                       A[31] &&  B[31] && !add_result[31] && ALUop == `ADD ||
+	                  !A[31] &&  B[31] &&  sub_result[31] && ALUop == `SUB ||
+	                   A[31] && !B[31] && !sub_result[31] && ALUop == `SUB  ;
 
 	assign Result = (ALUop == `AND) ? and_result :
 					(ALUop == `OR ) ? or_result  :
 					(ALUop == `ADD) ? add_result :
 					(ALUop == `SUB) ? sub_result :
 					(ALUop == `SLT) ? slt_result :
-					 				  32'b0      ;
+                                      32'b0      ;
 
 	assign Zero = Result == 32'b0 ? 1'b1 : 1'b0;
 
