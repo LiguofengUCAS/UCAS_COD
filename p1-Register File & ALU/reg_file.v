6eab19c5bc6a517a454c95509b52dcae85086588
LiguofengUCAS <liguofeng18@mails.ucas.ac.cn>
2021-04-05 11:05:45 +0800

    advance logic

--- p1-Register File & ALU/reg_file.v
+++ p1-Register File & ALU/reg_file.v
@@ -5,29 +5,28 @@
 `define NUM        32
 
 module reg_file(
-	input 					   clk,
-	input 					   rst,
-	input  [`ADDR_WIDTH - 1:0] waddr,
-	input  [`ADDR_WIDTH - 1:0] raddr1,
-	input  [`ADDR_WIDTH - 1:0] raddr2,
-	input  					   wen,
-	input  [`DATA_WIDTH - 1:0] wdata,
-	output [`DATA_WIDTH - 1:0] rdata1,
-	output [`DATA_WIDTH - 1:0] rdata2
+    input                      clk,
+    input                      rst,
+    input  [`ADDR_WIDTH - 1:0] waddr,
+    input  [`ADDR_WIDTH - 1:0] raddr1,
+    input  [`ADDR_WIDTH - 1:0] raddr2,
+    input                      wen,
+    input  [`DATA_WIDTH - 1:0] wdata,
+    output [`DATA_WIDTH - 1:0] rdata1,
+    output [`DATA_WIDTH - 1:0] rdata2
 );
-
-	reg [`DATA_WIDTH - 1:0]REG_Files[0:`NUM - 1];
+    
+    // No rst & No init
+    reg [`DATA_WIDTH - 1:0] REG_Files [0:`NUM - 1];
 	
-	always@(posedge clk)
-	begin
-		if(rst)
-			REG_Files[0] <= 0;
-		else
-			if(wen && waddr!=0)
-				REG_Files[waddr] <= wdata;
-	end
+    always@(posedge clk)
+    begin
+        if(wen && waddr)
+            REG_Files[waddr] <= wdata;
+    end
 
-assign rdata1 = REG_Files[raddr1];
-assign rdata2 = REG_Files[raddr2];
+    assign rdata1 = {32{raddr1 != 5'b0}} & REG_Files[raddr1];
+
+    assign rdata2 = {32{raddr2 != 5'b0}} & REG_Files[raddr2];
 
 endmodule
